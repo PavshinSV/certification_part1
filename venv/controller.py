27bc40fb602940b79509ea210b102cf5323921d3
PSV <pavshin@mail.ru>
2023-02-23 19:49:02 +0700

    menu created and controllers plug

---
+++ venv/controller.py
@@ -0,0 +1,23 @@
+notes = []
+id = 0
+
+def new_note():
+    pass
+
+def show_all():
+    pass
+
+def show_range(start_date,end_date):
+    pass
+
+def edit_note(id):
+    pass
+
+def remove_note(id):
+    pass
+
+def import_notes():
+    pass
+
+def export_notes():
+    pass
